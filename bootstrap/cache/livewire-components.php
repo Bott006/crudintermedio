a9a9f5fa390e5523bffc37fdaeab3271619ee92e
Bott006 <92000576+Bott006@users.noreply.github.com>
2022-09-17 03:32:09 -0500

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'articulos' => 'App\\Http\\Livewire\\Articulos',
+  'deptos' => 'App\\Http\\Livewire\\Deptos',
+  'doctores' => 'App\\Http\\Livewire\\Doctores',
+  'empresas' => 'App\\Http\\Livewire\\Empresas',
+  'estudios' => 'App\\Http\\Livewire\\Estudios',
+  'facturas' => 'App\\Http\\Livewire\\Facturas',
+  'pacientes' => 'App\\Http\\Livewire\\Pacientes',
+);
